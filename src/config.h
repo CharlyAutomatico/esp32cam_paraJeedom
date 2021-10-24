bbea6ffcb7d914b6b8cf712440c826d85d2d9e7d
Charly <charlyp@scual.es>
2021-10-24 02:32:47 +0200

    init

---
+++ src/config.h
@@ -0,0 +1,67 @@
+#define _DEBUG_ //debug
+//#define mDNS_CAM    // activar Nombre DNS
+
+#define LED_BUILTIN 4 // WILL BE USED TO TURN THE LED ON AND OFF - DO NOT MODIFY THIS VALUE (GPIO4)
+#define pinLED 2 //Led rojo
+//#define DHT //descomentar si utilizamos el sensor de temperatura DHT22
+#ifdef DHT
+// Data DHT22
+#define DHTPIN 14 // Digital pin connected to the DHT sensor
+// #define DHTTYPE DHT22   // DHT 22  (AM2302)
+#endif
+
+#define BME_ON //Activa  Sensor de temperatura
+
+//Timer send PUB MQTT
+#define intPublicMQTT 30
+//const byte intPublicMQTT = XX; //execution time in sec
+
+//PIR
+#define pinPIR 13
+//#define pinLed 33
+
+#define ENABLE_WEBSERVER          // Web Server
+#define RESOLUTION FRAMESIZE_SVGA // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+#define QUALITY 10                // JPEG quality 10-63 (lower means better quality)
+#define WSPORT 80                 //Web server port
+
+#define PWDN_GPIO_NUM 32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM 0
+#define SIOD_GPIO_NUM 26
+#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM 35
+#define Y8_GPIO_NUM 34
+#define Y7_GPIO_NUM 39
+#define Y6_GPIO_NUM 36
+#define Y5_GPIO_NUM 21
+#define Y4_GPIO_NUM 19
+#define Y3_GPIO_NUM 18
+#define Y2_GPIO_NUM 5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM 23
+#define PCLK_GPIO_NUM 22
+
+// PARAMETRES IMAGE
+#define BRIGHT 2   // Brightness: -2 to 2
+#define CONTRAST 0 // Contrast: -2 to 2
+#define SATUR 0    // Color Saturation: -2 to 2
+//#define SPECIAL   0           // Special effects: 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+#define WHITEBAL 1 // White Balance: 0 = disable , 1 = enable
+#define AWBGAIN 1  // White Balance Gain: 0 = disable , 1 = enable
+#define AWBMODE 0  // (3) White Balance Mode: 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+#define EXPOS 1    // Exposure Control: 0 = disable , 1 = enable
+#define AEC2 0     // 0 = disable , 1 = enable
+#define AELEV 0    // -2 to 2
+#define AECVAL 300 // 0 to 1200
+#define GAINCT 1   // 0 = disable , 1 = enable
+#define GAINAGC 0  // 0 = disable , 1 = enable
+#define GAINCELL 0 // 0 = disable , 1 = enable
+#define BPC 0      // 0 = disable , 1 = enable
+#define WPC 1      // 0 = disable , 1 = enable
+#define RAWGMA 1   // 0 = disable , 1 = enable
+#define LENC 1     // Lens Correction: 0 = disable , 1 = enable
+#define MIRROR 1   // Horizontal Mirror: 0 = disable , 1 = enable // 0 para lentes 160
+#define FLIP 0     // Vertical Flip: 0 = disable , 1 = enable // 0 para lentes 160
+#define DCW 1      // 0 = disable , 1 = enable
+#define CBAR 0     // Set a colorBar: 0 = disable , 1 = enable
---
+++ src/main.cpp
@@ -0,0 +1,473 @@
+//https : //techtutorialsx.com/2020/06/10/esp32-camera-image-server/
+
+#include "config.h"
+#include <Arduino.h>
+#include "WiFi.h"
+#include <ESP32Ping.h>
+#include "ESPAsyncWebServer.h"
+#include <WiFiClient.h>
+#include "esp_camera.h"
+#include "img_converters.h"
+#include "soc/soc.h"          //disable brownout problems
+#include "soc/rtc_cntl_reg.h" //disable brownout problems
+#include "time.h"             //para el calculo del tiempo NTP
+#include <PubSubClient.h> //PubSub messages
+const char *ssid = "MyDeco01";
+const char *password = "segundaplanta";
+
+// Calculo del tiempo NTP
+const char *ntpServer = "pool.ntp.org";
+const long gmtOffset_sec = 3600;
+const int daylightOffset_sec = 3600;
+
+#ifdef BME_ON
+#include <Wire.h>
+#include "SparkFunBME280.h"
+#define I2C_SDA 14 // SDA Connected to GPIO 14
+#define I2C_SCL 15 // SCL Connected to GPIO 15
+BME280 bme;
+float temperatura, humedad;
+float correccion_temp = 6;
+#endif
+
+// Data Timer para enviar los mensajes PUB
+#include <Ticker.h> //Timer
+Ticker publicMQ;      //Objeto Timer
+boolean sentinelSend = false;
+Ticker blinkLED; //Objeto Timer
+Ticker blinkLED1; //Objeto Timer
+//-----------------------
+
+// Datos MQTT
+//PUB cambiar test x ubicacion
+const char *mqtt_server = "192.168.1.185";
+const char *mqtt_topic_mensaje = "test/test2/mensajeCam";
+const char *mqtt_topic_temp = "test/test2/temperatura";
+//const char *mqtt_topic_hum = "test/test2/humedad";
+const char *mqtt_topic_pir = "test/test2/pir";
+//SUB
+const char *mqtt_subtopic_mensaje = "test/test/inESP32CAM";
+//------------------------
+
+//PIR
+boolean pirSensor;
+char pir[50]; //buffer para almacenar mensajes mqtt
+long int valuePir = 0;
+
+// Temporizador Detetector PIR
+unsigned long ultimaDeteccionPir = 0;
+unsigned long tiempoDeteccionPir = 6000;
+
+//WifiReconect = ESP.restart
+unsigned long previousMillis = 0;
+unsigned long interval = 30000;
+
+// Conexión a la red WiFi +  Broker
+WiFiClient espClient;
+PubSubClient clientMQ(espClient);
+
+//Parametros para gestion mensajes MQTT PUB
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+long int value = 0;
+char tiempoNTP[25];
+//-----------------------
+
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if (!getLocalTime(&timeinfo))
+  {
+    Serial.println("Failed to obtain time");
+    return;
+  }
+  // Serial.println(&timeinfo, "%Y-%m-%d %H:%M:%S");
+  strftime(tiempoNTP, sizeof(tiempoNTP), "%Y-%m-%d %H:%M:%S", &timeinfo);
+  String asString(tiempoNTP);
+}
+
+// // static void IRAM_ATTR detectsMovement(void *arg)
+// // {
+// //   Serial.println("Interrupt");
+// //   pirSensor = true;
+// // }
+
+// // void enableInterrupt()
+// // {
+// //   esp_err_t  err = gpio_isr_handler_add(GPIO_NUM_13, &detectsMovement, (void *)13);
+// //   if (err != ESP_OK)
+// //   {
+// //     Serial.printf("handler add failed with error 0x%x \r\n", err);
+// //   }
+
+// //   err = gpio_set_intr_type(GPIO_NUM_13, GPIO_INTR_POSEDGE);
+// //   if (err != ESP_OK)
+// //   {
+// //     Serial.printf("set intr type failed with error 0x%x \r\n", err);
+// //   }
+// // }
+
+AsyncWebServer server(WSPORT);
+
+bool initCamera()
+{
+  camera_config_t config;
+
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_SVGA;
+  config.jpeg_quality = QUALITY;
+  config.fb_count = 1;
+  // if (psramFound())
+  // {
+  //   config.frame_size = RESOLUTION; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
+  //   config.jpeg_quality = QUALITY;
+  //   config.fb_count = 2;
+  // }
+  // else
+  // {
+  //   if (RESOLUTION > FRAMESIZE_SVGA)
+  //   {
+  //     config.frame_size = FRAMESIZE_SVGA;
+  //   }
+  //   config.jpeg_quality = 12;
+  //   config.fb_count = 1;
+  // }
+
+  esp_err_t result = esp_camera_init(&config);
+
+  // delay(1000);
+  if (result != ESP_OK)
+  {
+    return false;
+  }
+  sensor_t *s = esp_camera_sensor_get();
+  s->set_brightness(s, BRIGHT);
+  s->set_contrast(s, CONTRAST);
+  s->set_saturation(s, SATUR);
+  // s->set_special_effect(s, SPECIAL);
+  // s->set_whitebal(s, WHITEBAL);
+  // s->set_awb_gain(s, AWBGAIN);
+  // s->set_wb_mode(s, AWBMODE);
+  // s->set_exposure_ctrl(s, EXPOS);
+  // s->set_aec2(s, AEC2);
+  // s->set_ae_level(s, AELEV);
+  // s->set_aec_value(s, AECVAL);
+  // s->set_gain_ctrl(s, GAINCT);
+  // s->set_agc_gain(s, GAINAGC);
+  // s->set_gainceiling(s, (gainceiling_t)GAINCELL);
+  // s->set_bpc(s, BPC);
+  // s->set_wpc(s, WPC);
+  // s->set_raw_gma(s, RAWGMA);
+  // s->set_lenc(s, LENC);
+  // s->set_hmirror(s, MIRROR);
+  // s->set_vflip(s, FLIP);
+  // s->set_dcw(s, DCW);
+  // s->set_colorbar(s, CBAR);
+
+  // enableInterrupt();
+  return true;
+}
+
+//Funcion TICKER enviar MQTT PUB cada x segundos (seg -> definido en intPublicMQTT)
+void callbackTimerMQ()
+{
+  sentinelSend = true;
+}
+void callbackTimerLED()
+{
+  // Cambiar de estado el LED
+  byte estado = digitalRead(pinLED);
+  digitalWrite(pinLED, !estado);
+}
+
+// Funciones MQTT ------------------------
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String messageMQ;
+
+  // Mostramos el mensaje en el monitor serie y lo asignamos a un String
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+    messageMQ += (char)payload[i];
+  }
+  Serial.println();
+
+  //Switch on the LED if an 1 was received as first character
+  if (messageMQ == "flashON")
+  {
+    digitalWrite(LED_BUILTIN, HIGH); //Flash LED
+    Serial.println("flashON");
+    //    digitalWrite(pinONPIR, HIGH);
+  }
+  else if (messageMQ == "flashOFF")
+  {
+    digitalWrite(LED_BUILTIN, LOW);
+    Serial.println("flashOFF");
+    //    digitalWrite(pinONPIR, LOW);
+  }
+  else if (messageMQ == "reset")
+  {
+    Serial.println("Reinicio ESP");
+    ESP.restart();
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!clientMQ.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Create a random client ID
+    String clientId = "ESP32Client-";
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (clientMQ.connect(clientId.c_str()))
+    {
+      Serial.println("connected");
+      // Once connected, publish an announcement...
+      clientMQ.publish(mqtt_topic_mensaje, "reconecto");
+      // ... and resubscribe
+      clientMQ.subscribe(mqtt_subtopic_mensaje);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(clientMQ.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void mqtt_mensaje()
+{
+  //mqtt_topic_mensaje
+  ++value;
+  printLocalTime();
+  snprintf(msg, MSG_BUFFER_SIZE, "hello test esp32Cam #%ld  %s", value, tiempoNTP);
+
+#ifdef _DEBUG_
+  Serial.print("Publish message: ");
+  Serial.println(msg);
+#endif
+  clientMQ.publish(mqtt_topic_mensaje, msg);
+}
+
+void mqtt_temp()
+{
+  //mqtt_topic_temp
+  //mqtt_topic_hum
+#ifdef DHT
+  // Read temperature as Celsius (the default) & Humidity
+  humedad = dht.getHumidity();
+  // humedad = dht.readHumidity();
+  temperatura = dht.getTemperature();
+  // temperatura = dht.readTemperature();
+#endif
+
+#ifdef BME_ON
+  temperatura = bme.readTempC();
+  temperatura = temperatura - correccion_temp;
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(temperatura))
+  {
+#ifdef _DEBUG_
+    Serial.println(F("Failed to read from DHT/BME sensor!"));
+#endif
+    return;
+  }
+
+#ifdef DHT
+  Serial.print(F("Humedad: "));
+  Serial.print(humedad);
+#endif
+  // Serial.print(F("%  Temperatura: "));
+  // Serial.println(temperatura);
+
+
+  static char temperatureTemp[7];
+  dtostrf(temperatura, 6, 2, temperatureTemp);
+#ifdef DHT
+  static char humidityTemp[7];
+  dtostrf(humedad, 6, 2, humidityTemp);
+#endif
+#ifdef _DEBUG_
+  Serial.print("Publish message: ");
+  Serial.print(temperatureTemp);
+  Serial.println(" C");
+#endif
+  clientMQ.publish(mqtt_topic_temp, temperatureTemp);
+#ifdef DHT
+  clientMQ.publish(mqtt_topic_hum, humidityTemp);
+#endif
+#endif
+}
+
+void mqtt_pir()
+{
+  if (pirSensor)
+  {
+    ++valuePir;
+    printLocalTime();
+    snprintf(pir, sizeof(pir), "Detectado movimiento #%ld  %s", valuePir, tiempoNTP);
+  }
+  else
+  {
+    snprintf(pir, sizeof(pir), "NO detecto movimiento");
+  }
+  Serial.print("Publish message: ");
+  Serial.println(pir);
+  clientMQ.publish(mqtt_topic_pir, pir);
+}
+//Fin Funciones MQTT-----------------------
+
+void setup()
+{
+#ifdef _DEBUG_
+  Serial.begin(115200);
+  // Serial.setDebugOutput(true);
+  // Serial.println();
+#endif
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+
+  pinMode(pinLED, OUTPUT); //LED rojo
+  pinMode(pinPIR, INPUT);  //PIR pin
+  pinMode(LED_BUILTIN, OUTPUT);   // flash LED
+  digitalWrite(LED_BUILTIN, LOW); // flash LED
+
+#ifdef BME_ON
+#ifdef _DEBUG_
+  Serial.println("Reading basic values from BME280");
+#endif
+  // Init BME280 sensor
+  Wire.begin(I2C_SDA, I2C_SCL, 400000);
+  bme.settings.commInterface = I2C_MODE;
+  bme.settings.I2CAddress = 0x76;
+  bme.settings.runMode = 3;
+  bme.settings.tStandby = 0;
+  bme.settings.filter = 0;
+  bme.settings.tempOverSample = 1;
+  bme.settings.pressOverSample = 1;
+  bme.settings.humidOverSample = 1;
+  bme.begin();
+#endif
+  // Setup Timer
+publicMQ.attach(intPublicMQTT, callbackTimerMQ);
+
+  if (!initCamera())
+  {
+
+    Serial.printf("Failed to initialize camera...");
+    return;
+  }
+  //Iniciamos conexion WIFI
+  WiFi.begin(ssid, password);
+  //iniciamos TemporizadorLED
+  blinkLED.attach(0.2, callbackTimerLED);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+
+  Serial.println(WiFi.localIP());
+  blinkLED.detach();
+  //init and get the time
+  configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
+  printLocalTime();
+
+  server.on("/picture", HTTP_GET, [](AsyncWebServerRequest *request)
+            {
+              camera_fb_t *frame = NULL;
+              frame = esp_camera_fb_get();
+              request->send_P(200, "image/jpeg", (const uint8_t *)frame->buf, frame->len);
+              esp_camera_fb_return(frame);
+            });
+
+  server.begin();
+
+  // Configuración MQTT
+  clientMQ.setServer(mqtt_server, 1883);
+  clientMQ.setCallback(callback);
+  //--------------------------
+}
+
+
+void loop()
+{
+
+  pirSensor = digitalRead(pinPIR);
+
+  if (pirSensor)
+  {
+    // Comprobar si se ha dado la vueltas
+    if (millis() < ultimaDeteccionPir)
+    {
+      // Asignar un nuevo valor
+      ultimaDeteccionPir = millis();
+    }
+    // Comprobar si ha pasado tiempo suficiente desde
+    // la última pulsación
+    if ((millis() - ultimaDeteccionPir) > tiempoDeteccionPir)
+    {
+      mqtt_pir();
+      // Marca de tiempo para nueva detección
+      ultimaDeteccionPir = millis();
+    }
+  }
+
+   // if WiFi is down, try reconnecting restart ESP
+  unsigned long currentMillis = millis();
+  if ((WiFi.status() != WL_CONNECTED) && (currentMillis - previousMillis >= interval))
+  {
+    Serial.println(millis());
+    Serial.println(WiFi.status());
+    Serial.println("Reiniciar ESP");
+    previousMillis = currentMillis;
+    ESP.restart();
+  }
+
+  // Comprobación si hemos perdido conexión mqtt
+  if (!clientMQ.connected())
+  {
+    reconnect();
+  }
+  // Procesamos los mensajes entrantes y  ejecutará la función callback que hayamos configurado
+  clientMQ.loop();
+  //Si ha pasado el tiempo marcado enviamos la temperatura
+  if (sentinelSend)
+  {
+    mqtt_mensaje();
+    mqtt_temp();
+    // mqtt_pir();
+    sentinelSend = false;
+  }
+}
